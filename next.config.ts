c9303a7b351329b8b995c4f71568e204ccd6faf4
Batsuuri <96187509+Batsuuri77@users.noreply.github.com>
2025-02-17 23:07:37 +1100

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Enables static site generation (SSG)
-  //assetPrefix: "./me", // Ensures assets work in GitHub Pages' environment
+  output: "export",
+  assetPrefix: "/me",
   images: {
-    unoptimized: true, // Disables image optimization (since it's not supported in GitHub Pages)
+    unoptimized: true,
   },
 };
 
