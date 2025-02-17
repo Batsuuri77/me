531da03c1370b311507e7b99bf2372f12ad52b31
Batsuuri <96187509+Batsuuri77@users.noreply.github.com>
2025-02-17 23:35:02 +1100

    repo name

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/your-repo-name", // Replace with your actual GitHub repo name
+  basePath: "/me", // Replace with your actual GitHub repo name
   trailingSlash: true, // Enable trailing slash
   images: {
     unoptimized: true,
