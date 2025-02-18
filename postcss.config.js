5892853a599efda4d13066cf5bd7d827774147ce
Batsuuri <96187509+Batsuuri77@users.noreply.github.com>
2025-02-18 20:18:02 +1100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+// postcss.config.mjs
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
